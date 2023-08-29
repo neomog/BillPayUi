2332e9dd1b61e2f0b93ecc694370babcb5c2eb50
Nathaniel Ohiare <ohiarenathanielmog@gmail.com>
2023-08-29 09:59:28 +0100

    refactor: delete folders

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BASE_URL: 'http://billpay.conmasol.com/'
+        BASE_URL: 'https://billpay.conmasol.com/'
     }
 }
 
